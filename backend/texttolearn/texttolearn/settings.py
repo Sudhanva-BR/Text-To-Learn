c7fbf1290c505eae02673e83fd495e2828bf044c
demo <sudhanva2025@gmailcom>
2025-11-05 10:15:17 +0530

    updated database settings for render PostgreSQL

--- backend/texttolearn/texttolearn/settings.py
+++ backend/texttolearn/texttolearn/settings.py
@@ -82,10 +82,18 @@ WSGI_APPLICATION = "texttolearn.wsgi.application"
 # -----------------------------------
 # Database
 # -----------------------------------
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=f"sqlite:///{BASE_DIR}/db.sqlite3",
+#         conn_max_age=600,
+#     )
+# }
+
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR}/db.sqlite3",
+        default=config("DATABASE_URL", default=f"sqlite:///{BASE_DIR}/db.sqlite3"),
         conn_max_age=600,
+        ssl_require=True  # ✅ important for Render PostgreSQL
     )
 }
 
